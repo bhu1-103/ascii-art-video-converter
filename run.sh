eaa7881a1ee445087a07d93a8126d7700a062fc9
bhu1-103 <103.bhuvan@gmail.com>
2025-01-24 23:37:54 +0530

    Fri Jan 24 11:37:54 PM IST 2025

--- run.sh
+++ run.sh
@@ -2,8 +2,10 @@
 
 yt-dlp $1 -o video
 mkdir -p frames
-ffmpeg -i video.webm -vf "fps=24" frames/frame_%05d.png
+ffmpeg -i video.webm -vf "fps=1" frames/frame_%05d.png
+echo "step 1 done"
+echo "downloading audio now"
 yt-dlp -x --audio-format mp3 $link -o audio.mp3
 python convert.py
 clear; sleep 1; mpv 
-mpv audio.mp3 & ./run.sh
+mpv audio.mp3 & ./display.sh
