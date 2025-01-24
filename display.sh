b63f3ebce40f7d1c69f0ee33e24880898302c029
bhu1-103 <103.bhuvan@gmail.com>
2025-01-24 23:42:47 +0530

    variable mismatch

--- display.sh
+++ display.sh
@@ -2,5 +2,6 @@
 for txt_file in "frames-t"/*.txt; do
 	clear
 	cat "$txt_file"
-	sleep 0.04
+	sleep 0.04 # for 24 fps
+	#sleep 1 # for 1 fps
 done
--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 yt-dlp $1 -o video
 mkdir -p frames
-ffmpeg -i video.webm -vf "fps=1" frames/frame_%05d.png
+ffmpeg -i video.webm -vf "fps=24" frames/frame_%05d.png
 echo "step 1 done"
 echo "downloading audio now"
 yt-dlp -x --audio-format mp3 $1 -o audio.mp3
