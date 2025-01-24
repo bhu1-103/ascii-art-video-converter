a00f89c655f8a75ec4545f761f95fd27488e919d
bhu1-103 <103.bhuvan@gmail.com>
2025-01-24 23:39:21 +0530

    variable mismatch

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ mkdir -p frames
 ffmpeg -i video.webm -vf "fps=1" frames/frame_%05d.png
 echo "step 1 done"
 echo "downloading audio now"
-yt-dlp -x --audio-format mp3 $link -o audio.mp3
+yt-dlp -x --audio-format mp3 $1 -o audio.mp3
 python convert.py
 clear; sleep 1; mpv 
 mpv audio.mp3 & ./display.sh
